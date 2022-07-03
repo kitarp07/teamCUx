cd35c599117794f939d14d5a06d131c2d8e28be1
Samyog Bohara <fallendemon174@gmail.com>
2022-07-03 13:37:23 +0545

    fixed create-form

--- testmyux/urls.py
+++ testmyux/urls.py
@@ -25,7 +25,7 @@ from pages.views import *
 from Tester.views import *
 urlpatterns = [
 #    path('admin', auth_views.LoginView.as_view(template_name='adminpage/adminlogin.html'), name='admin'),
-   path('admin/', admin.site.urls),
+   path('sadmin/', admin.site.urls),
    path('client/register', client_reg_view, name="client-reg" ),
    path('', homepage, name="homepage" ),
    path('',include('Tester.urls')),
